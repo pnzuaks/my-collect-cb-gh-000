5c4c5d28b80b830c0ce0e67f67f486955a1738fe
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-12 23:58:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(collection)
+  i = 0 
+  array = []
+  while i < collection.length
+  yield(collection[i])
+  array << yield(collection[i])
+  i += 1
+ 
+  
+end
+
+  array
+end
+
+
+
+
+
 
 
